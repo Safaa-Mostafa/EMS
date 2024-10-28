0dda9f842c1bc665881175512608b1c72a8e722e
mohamedAyman00 <ma1090560@gmail.com>
2024-10-28 17:27:57 +0300

    assets
    add assets foldedr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,TableModule],
+  imports: [RouterOutlet,FormsModule,TableModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
