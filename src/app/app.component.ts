ca4db5fd3b7b59cdcdbc00f402f1a7b4fbbd545a
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2024-10-28 16:54:26 +0300

    edit_on_app_config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,15 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { TableModule } from 'primeng/table';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,TableModule],
+  imports: [RouterOutlet,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EMS_Dashboard';
-  checked = true;
-  products: any[] = [];
-
-  constructor() {
-    this.loadProducts();
-  }
-
-  loadProducts() {
-    // Replace this with your actual data fetching logic
-    this.products = [
-      { code: 'P001', name: 'Product 1', category: 'Category 1', quantity: 10 },
-      { code: 'P002', name: 'Product 2', category: 'Category 2', quantity: 20 },
-      { code: 'P003', name: 'Product 3', category: 'Category 3', quantity: 30 },
-      // Add more products as needed
-    ];
-  }
 }
+
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { SidebarModule } from 'primeng/sidebar';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SidebarModule, RippleModule, AvatarModule, StyleClassModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  dismissable = true;
 
+
+  sidebarVisible: boolean = true;
 }
