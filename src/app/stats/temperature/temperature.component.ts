d1b99751dcd59a524f641348a2166387f61416b9
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2024-10-29 18:43:23 +0300

    addTempretureSection

--- src/app/stats/temperature/temperature.component.ts
+++ src/app/stats/temperature/temperature.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { ChartModule } from 'primeng/chart';
 @Component({
   selector: 'app-temperature',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './temperature.component.html',
   styleUrl: './temperature.component.css'
 })
 export class TemperatureComponent {
+  temperatureData: any = {
+    labels: ['Ground Floor', 'First Floor', 'Second Floor', 'Third Floor'],
+    datasets: [
+      {
+        label: 'Temperature (°C)',
+        data: [24, 22, 22, 23], // Example data
+        backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726', '#26C6DA']
+      }
+    ]
+  };
 
+  chartOptions: any = {
+    responsive: true,
+    indexAxis: 'y',  // Set indexAxis to 'y' to make the chart horizontal
+    scales: {
+      x: {
+        ticks: { color: '#ffffff' }
+      },
+      y: {
+        ticks: { color: '#ffffff' }
+      }
+    },
+    plugins: {
+      legend: { display: true, labels: { color: '#ffffff' } }
+    }
+  };
 }
