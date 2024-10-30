470afeba1932fb5e7199fbbc5d1c4f1b2d9b3fa1
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2024-10-31 00:39:24 +0300

    add systemcomponents

--- src/app/systems/chiller-system/chiller-system.component.ts
+++ src/app/systems/chiller-system/chiller-system.component.ts
@@ -1,12 +1,13 @@
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chiller-system',
   standalone: true,
-  imports: [],
+  imports: [ProgressSpinnerModule],
   templateUrl: './chiller-system.component.html',
   styleUrl: './chiller-system.component.css'
 })
 export class ChillerSystemComponent {
-
+value:number=20;
 }
