2d59fac48ab7f67a3dd4dc064aa1cb09b411df83
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2024-10-30 17:09:58 +0300

    add_floor_status

--- src/app/ahu/floor-ahu-status/floor-ahu-status.component.ts
+++ src/app/ahu/floor-ahu-status/floor-ahu-status.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-floor-ahu-status',
   standalone: true,
-  imports: [],
+  imports: [ChartModule, CardModule,BadgeModule],
   templateUrl: './floor-ahu-status.component.html',
   styleUrl: './floor-ahu-status.component.css'
 })
 export class FloorAhuStatusComponent {
-
+  floors = [
+    { name: 'Ground Floor', on: 7, off: 3 },
+    { name: 'First Floor', on: 4, off: 6 },
+    { name: 'Second Floor', on: 8, off: 2 },
+    { name: 'Third Floor', on: 6, off: 4 }
+  ];
+  
 }
